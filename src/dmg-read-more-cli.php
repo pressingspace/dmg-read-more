dca16095e9cc70ad0752dad0189dff60ac983b02
Stuart McAlpine <stu@pressingspace.com>
2024-05-06 20:33:22 +0100

    Descriptive comments

--- src/dmg-read-more-cli.php
+++ src/dmg-read-more-cli.php
@@ -33,28 +33,31 @@ class DMG_ReadMoreCLI extends WP_CLI_Command
 
     $date_query = [];
 
+    // Use CLI arg for date "before" query param
     if ( ! empty( $assoc_args['date-before'] ) ) {
         $date_query['before'] = $assoc_args['date-before'];
     }
 
+    // Use CLI arg for date "after" query param
     if ( ! empty( $assoc_args['date-after'] ) ) {
         $date_query['after'] = $assoc_args['date-after'];
     }
 
+    // Fallback to last 30 days (including today)
     if ( empty( $date_query ) ) {
         $date_query['before'] = 'tomorrow';
         $date_query['after']  = 'tomorrow - 30 days';
     }
 
-    // @todo Consider limiting date query to a max length e.g. 365 days
+    /**
+     * @todo Consider limiting date query to a max length e.g. 365 days
+     */
     $query_params['date_query'] = $date_query;
 
     /**
-     * @todo switch WIP "?s" query for MySQL LIKE query on post content
-     * (there is no need to search posts titles)
+     * Filters the WHERE clause of the query.
      *
-     * e.g. filter wp_query for our CLI command using
-     * https://developer.wordpress.org/reference/hooks/pre_get_posts/
+     * Provides an efficient method to query for posts that contain our block.
      */
     $filterQuery = function( $where, \WP_Query $query ) {
         global $wpdb;
