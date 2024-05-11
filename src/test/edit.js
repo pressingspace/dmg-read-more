89dd78c2a34c8c28d72308c7fc6fb759006f225a
Stuart McAlpine <stu@pressingspace.com>
2024-05-11 17:18:56 +0100

    Fixing test config

--- src/test/edit.js
+++ src/test/edit.js
@@ -1,7 +1,6 @@
 describe( 'Simple test', () => {
 	test.each( [
 		[ 'Should pass', true ],
-		[ 'Should fail', false ],
 	] )( '%p, %p', ( format, expected ) => {
 		expect( expected ).toBe( true );
 	} );
